8c35754c66e3ee11fd42e6e716fe879209d0df41
wjdwodnd93 <jjw_0609@naver.com>
2024-12-16 20:54:12 +0900

    페이지 처리를 위한 DTO 설계

--- src/main/java/org/jjw/reactmallapi/dto/PageRequestDTO.java
+++ src/main/java/org/jjw/reactmallapi/dto/PageRequestDTO.java
@@ -1,10 +1,16 @@
 package org.jjw.reactmallapi.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 
 @SuperBuilder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PageRequestDTO {
 
     @Builder.Default
