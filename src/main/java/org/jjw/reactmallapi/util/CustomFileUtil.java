1468ff82717b0d49a4d1b11953ce531242033849
wjdwodnd93 <jjw_0609@naver.com>
2025-02-03 20:42:42 +0900

    파일 업로드 설정

---
+++ src/main/java/org/jjw/reactmallapi/util/CustomFileUtil.java
@@ -0,0 +1,37 @@
+package org.jjw.reactmallapi.util;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+@Component
+@Log4j2
+@RequiredArgsConstructor
+public class CustomFileUtil {
+
+    @Value("${org.zerock.upload.path}")
+    private String uploadPath;
+
+    @PostConstruct
+    public void init() {
+        File tempFolder = new File(uploadPath);
+
+        if(!tempFolder.exists()) {
+            tempFolder.mkdirs();
+        }
+
+        uploadPath = tempFolder.getAbsolutePath();
+    }
+
+    public List<String> saveFiles(List<MultipartFile> files) throws RuntimeException {
+
+        return null;
+    }
+}
