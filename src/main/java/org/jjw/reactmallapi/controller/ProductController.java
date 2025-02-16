4fb7498b387cc05855f1077ceb46adeea622b1d0
wjdwodnd93 <jjw_0609@naver.com>
2025-02-16 22:19:18 +0900

    파일업로드 썸네일 처리

---
+++ src/main/java/org/jjw/reactmallapi/controller/ProductController.java
@@ -0,0 +1,39 @@
+package org.jjw.reactmallapi.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.jjw.reactmallapi.dto.ProductDTO;
+import org.jjw.reactmallapi.util.CustomFileUtil;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.Map;
+
+@Log4j2
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private final CustomFileUtil fileUtil;
+
+    @PostMapping("/")
+    public Map<String, String> register(ProductDTO productDTO) {
+
+        log.info("register: {}", productDTO);
+
+        List<MultipartFile> files = productDTO.getFiles();
+
+        List<String> uploadedFileNames = fileUtil.saveFiles(files);
+
+        productDTO.setUploadedFileNames(uploadedFileNames);
+
+        log.info(uploadedFileNames);
+
+        return Map.of("RESULT", "SUCCESS");
+    }
+}
