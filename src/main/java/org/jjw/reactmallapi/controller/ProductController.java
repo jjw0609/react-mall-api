42de52bdda450c3d340a53ede3ed5a1680a59f2e
wjdwodnd93 <jjw_0609@naver.com>
2025-02-17 22:04:49 +0900

    업로드파일 조회

--- src/main/java/org/jjw/reactmallapi/controller/ProductController.java
+++ src/main/java/org/jjw/reactmallapi/controller/ProductController.java
@@ -5,9 +5,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.jjw.reactmallapi.dto.ProductDTO;
 import org.jjw.reactmallapi.util.CustomFileUtil;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -35,5 +35,11 @@ public class ProductController {
         log.info(uploadedFileNames);
 
         return Map.of("RESULT", "SUCCESS");
+    }
+
+    @GetMapping("/view/{fileName}")
+    public ResponseEntity<Resource> viewFileGET(@PathVariable String fileName) {
+
+        return fileUtil.getFile(fileName);
     }
 }
