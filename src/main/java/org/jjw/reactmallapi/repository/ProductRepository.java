033db5b7c1cc7eb6a16cfefbec378f180b658261
wjdwodnd93 <jjw_0609@naver.com>
2025-03-02 00:17:49 +0900

    엔티티 CRUD

--- src/main/java/org/jjw/reactmallapi/repository/ProductRepository.java
+++ src/main/java/org/jjw/reactmallapi/repository/ProductRepository.java
@@ -1,9 +1,23 @@
 package org.jjw.reactmallapi.repository;
 
 import org.jjw.reactmallapi.domain.Product;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 
+    @EntityGraph(attributePaths = "imageList")
+    @Query("select p from Product p where p.pno = :pno")
+    Optional<Product> selectOne( @Param("pno)") Long pno );
+
+    @Modifying
+    @Query("update Product p set p.delFlag = :delFlag where p.pno = :pno")
+    void updateToDelete( @Param("pno)") Long pno, @Param("delFlag") boolean flag );
 }
--- src/test/java/org/jjw/reactmallapi/repository/ProductRepositoryTests.java
+++ src/test/java/org/jjw/reactmallapi/repository/ProductRepositoryTests.java
@@ -5,7 +5,10 @@ import org.jjw.reactmallapi.domain.Product;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @SpringBootTest
@@ -21,6 +24,63 @@ public class ProductRepositoryTests {
 
         product.addImageString(UUID.randomUUID() + "_" + "IMAGE1.jpg");
         product.addImageString(UUID.randomUUID() + "_" + "IMAGE2.jpg");
+
+        productRepository.save(product);
+    }
+
+    @Transactional
+    @Test
+    public void testRead() {
+
+        Long pno = 1L;
+
+        Optional<Product> result = productRepository.findById(pno);
+
+        Product product = result.orElseThrow();
+
+        log.info(product);
+
+        log.info(product.getImageList());
+    }
+
+    @Test
+    public void testRead2() {
+
+        Long pno = 1L;
+
+        Optional<Product> result = productRepository.selectOne(pno);
+
+        Product product = result.orElseThrow();
+
+        log.info(product);
+
+        log.info(product.getImageList());
+    }
+
+    @Commit
+    @Transactional
+    @Test
+    public void testDelete() {
+
+        Long pno = 2L;
+
+        productRepository.updateToDelete(pno, true);
+    }
+
+    @Test
+    public void testUpdate() {
+
+        Product product = productRepository.selectOne(1L).get();
+
+        product.changePrice(3000);
+
+        product.clearList();
+
+        product.addImageString(UUID.randomUUID() + "_" + "PIMAGE1.jpg");
+
+        product.addImageString(UUID.randomUUID() + "_" + "PIMAGE2.jpg");
+
+        product.addImageString(UUID.randomUUID() + "_" + "PIMAGE3.jpg");
 
         productRepository.save(product);
     }
